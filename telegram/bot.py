6022b51ec7ef97fcea3640a3b59897ef3f0a8630
anatolytsi <tsirkunenkoat@gmail.com>
2022-06-25 00:02:07 +0200

    fixed telegram channel to token

--- telegram/bot.py
+++ telegram/bot.py
@@ -1,5 +1,6 @@
-from communication.base import CHANNEL_KEY, DATA_KEY, BusDir
+from communication.base import DATA_KEY, BusDir
 from communication.mixins import BusMixin
+from db.website import TOKEN_KEY
 from telegram.base import TelegramBotMixin
 
 
@@ -8,4 +9,4 @@ class TelegramBot(BusMixin, TelegramBotMixin):
         super().__init__(*args, sub_dir=BusDir.COM, pub_dir=BusDir.TG, **kwargs)
 
     async def on_bus_message(self, message: any):
-        return await self.send_tg_message(message[DATA_KEY][CHANNEL_KEY], message[DATA_KEY])
+        return await self.send_tg_message(message[DATA_KEY][TOKEN_KEY], message[DATA_KEY])
