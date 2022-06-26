255de368836a7b9dff84a16bf95ed19c6c4c09ec
anatolytsi <tsirkunenkoat@gmail.com>
2022-06-26 13:06:24 +0200

    added wrong reply handling

--- telegram/bot.py
+++ telegram/bot.py
@@ -28,15 +28,20 @@ class TelegramBot(BusMixin, TelegramBotMixin):
         return await self.send_tg_message(message.data[TOKEN_KEY], '\n'.join([token_id, session_id, user, text]))
 
     async def handle_reply(self, message: types.Message):
-        parent_text = message.reply_to_message.text
-        token_end = re.search(f'^{TOKEN_TXT}([^\n]+)', parent_text, re.M).group(1)
-        session_key_end = re.search(f'^{SESSION_TXT}([^\n]+)', parent_text, re.M).group(1)
-        token = get_full_token(token_end)
-        session_key = get_full_session_key(token, session_key_end)
-        data = ChatMessage(token=token,
-                           text=message.text,
-                           timestamp=int(time.time()),
-                           session=session_key,
-                           user=message.from_user.first_name,
-                           username=message.from_user.username)
-        self.send_bus_message(token, data.to_dict())
+        try:
+            parent_text = message.reply_to_message.text
+            token_end = re.search(f'^{TOKEN_TXT}([^\n]+)', parent_text, re.M).group(1)
+            session_key_end = re.search(f'^{SESSION_TXT}([^\n]+)', parent_text, re.M).group(1)
+            token = get_full_token(token_end)
+            session_key = get_full_session_key(token, session_key_end)
+            data = ChatMessage(token=token,
+                               text=message.text,
+                               timestamp=int(time.time()),
+                               session=session_key,
+                               user=message.from_user.first_name,
+                               username=message.from_user.username)
+            self.send_bus_message(token, data.to_dict())
+        except AttributeError:
+            await self._bot.send_message(chat_id=message.from_user.id,
+                                         text='Message you replied to is incorrect and does not belong to any chat',
+                                         disable_notification=True)
